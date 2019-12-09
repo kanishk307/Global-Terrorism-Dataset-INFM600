6079734ec066f5708ae9a9dcb498eaed34495150
kanishk307 <ysj30797.kj@gmail.com>
2019-12-09 17:56:36 -0500

    Delete Taliban.r

--- Code/Taliban.r
+++
@@ -1,4 +0,0 @@
-newgtd = read.csv(file.choose()) #Load the dataset
-taliban = subset(newgtd,newgtd$gname=="Taliban") #Create a subset
-plot(taliban$country_txt) #A bar graph (Later made in Tableau)
-write.csv(taliban,"taliban.csv") #Exporting to csv
